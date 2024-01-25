bd252ee3b20f51f9ca95e61ef274796bba278ce0
Vishal-m <vishalmahesh.study@gmail.com>
2024-01-25 19:17:33 +0530

    db: resetPasswordSchema

--- components/auth/ResetPasswordForm.tsx
+++ components/auth/ResetPasswordForm.tsx
@@ -2,7 +2,7 @@
 
 import { ErrorMessage, SuccessMessage } from "@/components/StatusMessage";
 import CardWrapper from "@/components/auth/CardWrapper";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,7 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { loginSchema } from "@/schemas";
+import { resetPasswordSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -20,50 +20,33 @@ import * as z from "zod";
 import { login } from "@/actions/login";
 import { Loader } from "lucide-react";
 import { useState, useTransition } from "react";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
 
-const LoginForm = () => {
+const ResetPasswordForm = () => {
   const [isError, setIsError] = useState<string | undefined>("");
   const [isSuccess, setIsSuccess] = useState<string | undefined>("");
 
-  const searchParams = useSearchParams();
-  const OAuthError =
-    searchParams.get("error") === "OAuthAccountNotLinked"
-      ? "Account already linked with another provider"
-      : "";
-  console.log("OAUTH", OAuthError);
-
-  const form = useForm<z.infer<typeof loginSchema>>({
-    resolver: zodResolver(loginSchema),
+  const form = useForm<z.infer<typeof resetPasswordSchema>>({
+    resolver: zodResolver(resetPasswordSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
 
   const [isPending, startTransition] = useTransition();
 
-  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+  const onSubmit = (values: z.infer<typeof resetPasswordSchema>) => {
     setIsError("");
     setIsSuccess("");
     startTransition(async () => {
-      login(values).then((res) => {
-        if (res.success) {
-          setIsSuccess(res.success);
-        } else {
-          setIsError(res.error);
-        }
-      });
+        // TODO: add server action
     });
   };
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonHref="/auth/register"
-      backButtonLabel="Don't have an account?"
-      showSocials={true}
+      headerLabel="Reset password"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login page"
     >
       <Form {...form}>
         <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
@@ -76,7 +59,7 @@ const LoginForm = () => {
                   <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
-                      placeholder="your-name@gmail.com"
+                      placeholder="wayne.bruce@gmail.com"
                       {...field}
                       type="email"
                     />
@@ -85,35 +68,13 @@ const LoginForm = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="*******" {...field} type="password" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
           </div>
-          <Link
-            className={buttonVariants({
-              variant: "link",
-              size: "sm", 
-              className: "px-0"
-            })}
-            href="/auth/reset-password"
-          >
-            Forgot password?
-          </Link>
+
           {isSuccess && <SuccessMessage label={isSuccess} />}
           {isError && <ErrorMessage label={isError} />}
-          {OAuthError && <ErrorMessage label={OAuthError} />}
+
           <Button disabled={isPending} className="w-full" type="submit">
-            Sign in
+            Send email
             {isPending && <Loader className="h-4 w-4 animate-spin" />}
           </Button>
         </form>
@@ -122,4 +83,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default ResetPasswordForm;
--- schemas/index.ts
+++ schemas/index.ts
@@ -12,3 +12,7 @@ export const registerSchema = z.object({
     .min(5, { message: "Password should be a min of 5 characters long" }),
   name: z.string().min(1, { message: "username is required" }),
 });
+
+export const resetPasswordSchema = z.object({
+  email: z.string().email({ message: "Enter a valid email" }),
+});
